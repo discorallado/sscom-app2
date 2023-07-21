bf60fdcdc4fdc9fe254b7f8341724753adbf7302
discorallado <sebastian.alvarez.c@usach.cl>
2023-07-21 07:45:18 -0400

    Update WorkResource.php

--- app/Filament/Resources/Manager/WorkResource.php
+++ app/Filament/Resources/Manager/WorkResource.php
@@ -156,35 +156,6 @@ class WorkResource extends Resource
       ->filters([
         Tables\Filters\TrashedFilter::make(),
 
-        Tables\Filters\Filter::make('created_at')
-          ->form([
-            Forms\Components\DatePicker::make('created_from')
-              ->placeholder(fn ($state): string => 'Dec 18, ' . now()->subYear()->format('Y')),
-            Forms\Components\DatePicker::make('created_until')
-              ->placeholder(fn ($state): string => now()->format('M d, Y')),
-          ])
-          ->query(function (Builder $query, array $data): Builder {
-            return $query
-              ->when(
-                $data['created_from'],
-                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
-              )
-              ->when(
-                $data['created_until'],
-                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
-              );
-          })
-          ->indicateUsing(function (array $data): array {
-            $indicators = [];
-            if ($data['created_from'] ?? null) {
-              $indicators['created_from'] = 'Order from ' . Carbon::parse($data['created_from'])->toFormattedDateString();
-            }
-            if ($data['created_until'] ?? null) {
-              $indicators['created_until'] = 'Order until ' . Carbon::parse($data['created_until'])->toFormattedDateString();
-            }
-
-            return $indicators;
-          }),
       ])
       ->actions([
         Tables\Actions\EditAction::make(),
